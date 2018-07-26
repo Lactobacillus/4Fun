3a99f4bccab8ccb3fe8c11b37bef3621fcf3acdf
Lactobacillus <cyw314@gmail.com>
2018-07-27 05:16:08 +0900

    good

--- BWT_pattern_matching/main.py
+++ BWT_pattern_matching/main.py
@@ -4,6 +4,7 @@ import argparse
 
 argParser = argparse.ArgumentParser(description = 'pattern matching using BWT (Burrows-Wheeler Transform)')
 argParser.add_argument('-s', '--string', required = True, help = 'example : panamabananas -> smnpbnnaaaaa$a')
+argParser.add_argument('-q', '--query', required = False, help = 'string for searching')
 
 def preprocess(string):
 
@@ -59,17 +60,132 @@ def printList(l):
 
 	print('')
 
-def main(original):
+def getSuffixArray(string):
 
-	original = preprocess(original)
+	stringList = list()
+	suffixArray = list()
+
+	for _ in range(len(string)):
+
+		stringList.append(string)
+		string = cyclicRotate(string)
+
+	stringList.sort()
+
+	for string in stringList:
+
+		suffixArray.append((len(string.split('$')[0]) + 1) % len(stringList))
+
+	return suffixArray
+
+def lfMapping(original, transformed):
+
+	lf = dict()
+
+	characters = set(transformed)
+
+	for char in characters:
+
+		mapping = list()
+
+		for idx in range(1, len(transformed) + 1):
+
+			mapping.append(transformed[:idx].count(char))
+
+		lf[char] = mapping
+
+	for key, val in lf.items():
+
+		print(key, ' : ', val)
+
+	print('')
+
+	return lf
+
+def findTop(stringList, char, top):
+
+	for idx in range(0, len(stringList)):
+
+		if stringList[idx][-1] == char and idx >= top:
+
+			return idx
+
+def findBottom(stringList, char, bottom):
+
+	for idx in range(len(stringList), 0, -1):
+
+		if stringList[idx - 1][-1] == char and idx - 1 <= bottom:
+
+			return idx - 1
+
+def findFront(stringList, char, pos):
+
+	count = 1
+
+	for idx, string in enumerate(stringList):
+
+		if string[0] == char:
+
+			if pos == count:
+
+				return idx
+
+			else:
+
+				count = count + 1
+
+def search(string, query, lf):
+
+	stringList = list()
+
+	for _ in range(len(string)):
+
+		stringList.append(string)
+		string = cyclicRotate(string)
+
+	stringList.sort()
+
+	top = 0
+	bottom = len(stringList)
+
+	for idx, char in enumerate(query[::-1]):
+
+		top = findFront(stringList, char, lf[char][findTop(stringList, char, top)])
+		bottom = findFront(stringList, char, lf[char][findBottom(stringList, char, bottom)])
+
+	return top, bottom
+
+def main(arg):
+
+	original = preprocess(arg.string)
 	transformed = BWT(original)
 	recovered = inverseBWT(transformed)
 
 	print('o : ', original)
 	print('t : ', transformed)
 	print('r : ', recovered)
+	print('')
+
+	if len(arg.query) > 0:
+
+		query = arg.query
+		lf = lfMapping(original, transformed)
+		searched = search(original, query, lf)
+
+	print(searched)
+	print('')
+
+	suffix = getSuffixArray(original)
+	print('suffix array : ', suffix)
+	print('')
+
+	for idx in range(searched[0], searched[1] + 1):
+
+		pos = len(original) - suffix[idx]
+		print('position : ', pos)
+		print(original[:pos] + ' ' + original[pos:pos + len(query)] + ' ' + original[pos + len(query):])
 
 if __name__ == '__main__':
 
 	arguments = argParser.parse_args()
-	main(arguments.string)
+	main(arguments)
