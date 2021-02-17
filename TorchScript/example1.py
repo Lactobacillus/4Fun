395d9dd08c3a525edf1cf98a5801071ace7d816f
Lactobacillus <cyw314@gmail.com>
2021-02-18 00:22:45 +0900

    init

---
+++ TorchScript/example1.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import timeit
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.jit as jit
+
+def main_1():
+
+	pass
+
+if __name__ == '__main__':
+
+
+	print(sys.version)
+	print(torch.__version__)
+
+	main_1()
